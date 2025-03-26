84acd26b9d4e252d542dfd84f030d96299095db9
Vigo28 <gamingvigo06@gmail.com>
2025-03-26 08:20:11 +0200

    post and user model(class) add return

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -11,7 +12,8 @@ class Post extends Model
 
     protected $fillable = ['title', 'body'];
 
-    public function user() {
-        $this->belongsTo(User::class);
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
