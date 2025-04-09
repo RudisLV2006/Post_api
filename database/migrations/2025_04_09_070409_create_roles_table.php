611e62ac9f693d9dfaf306f3b0a44c24e3e60c52
Vigo28 <gamingvigo06@gmail.com>
2025-04-09 10:13:26 +0300

    Fix table to create

--- database/migrations/2025_04_09_070409_create_roles_table.php
+++ database/migrations/2025_04_09_070409_create_roles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->timestamps();
